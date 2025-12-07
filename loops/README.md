00a9cb4199047d4417b6c1b7462aad24d38bc42e
Shubhanjali801 <shubhanjali32@gmail.com>
2025-12-07 22:38:21 +0530

    add : build a missing-letter-dectator a-profile-lookup a-string-repeating-function files and modify readme file

---
+++ loops/README.md
@@ -0,0 +1,1 @@
+### [Review-javascript-loops](https://www.freecodecamp.org/learn/javascript-v9/review-javascript-loops/review-javascript-loops)
---
+++ loops/build-a-missing-letter-dectator.js
@@ -0,0 +1,11 @@
+function fearNotLetter(str) {
+  for (let i = 0; i < str.length - 1; i++) {
+    let current = str.charCodeAt(i);
+    let next = str.charCodeAt(i + 1);
+
+    if (next - current !== 1) {
+      return String.fromCharCode(current + 1);
+    }
+  }
+  return undefined;
+}
